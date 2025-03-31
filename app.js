2a455f89e631cf094ceac8741e811a208a9c871f
Luan <luanlutii@gmail.com>
2025-03-31 19:53:17 -0300

    Alert resultado

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 
 let nomeCompleto = ""
 nomeCompleto = prompt("Informe seu nome completo ")
-console.log(nomeCompleto)
 
-let cor = prompt("informe uma cor: \n 1 azul \n2 vermelho \n3 preto \n4 vermelho \n5 rosa \n6 laranja")
-let corDeFundo = null
+
+function fnTrocarCorDeFundo(){
+ let cor = prompt("informe uma cor: \n 1 azul \n2 vermelho \n3 preto \n4 vermelho \n5 rosa \n6 laranja")
+ let corDeFundo = null
  
-if(cor == "1"){
+ if(cor == "1"){
     corDeFundo = "blue"
-}else if(cor == "2"){
+ }else if(cor == "2"){
     corDeFundo = "red"
-}else if(cor == "3"){
+ }else if(cor == "3"){
     corDeFundo = "black"
-}else if(cor == "4"){
+ }else if(cor == "4"){
     corDeFundo = "tomato"
-}else if(cor == "5"){
+ }else if(cor == "5"){
     corDeFundo = "pink"
-}else if(cor == "6"){
+ }else if(cor == "6"){
     corDeFundo = "orange"
-}else{
+ }else{
     corDeFundo = "yellow"
-}
-document.body.style.backgroundColor = corDeFundo
+ }
+ document.body.style.backgroundColor = corDeFundo
+}  
+
+fnTrocarCorDeFundo()
---
+++ exercicio-01/ex01.js
@@ -0,0 +1,5 @@
+
+let num1 = prompt("Digite um numero")
+let num2 = prompt("Digite um segundo numero")
+let resultado = parseFloat(num1) + parseFloat(num2)
+alert("O resultado é: " + resultado)
