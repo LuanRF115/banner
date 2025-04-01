d152f2c3edf57381c3a0f6fda15b3c0451b6bffe
Luan <luanlutii@gmail.com>
2025-03-31 21:43:09 -0300

    exercício 05

---
+++ exercicio-05/ex05.js
@@ -0,0 +1,5 @@
+let celsius = parseFloat(prompt("Digite a temperatura em Celsius:"));
+
+let fahrenheit = 1.8 * celsius + 32;
+
+alert("A temperatura de " + celsius + "°C é igual a " + fahrenheit + "°F.");
