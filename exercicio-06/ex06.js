7a13ce66edded771b10cf9eaae529770f0cbdb76
Luan <luanlutii@gmail.com>
2025-03-31 21:51:08 -0300

    exercício 06

---
+++ exercicio-06/ex06.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt("Digite um número:"));
+
+let antecessor = numero - 1;
+let sucessor = numero + 1;
+
+alert("O antecessor do número " + numero + " é " + antecessor + ", e o sucessor é " + sucessor + ".");
